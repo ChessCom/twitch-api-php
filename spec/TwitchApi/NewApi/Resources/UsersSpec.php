d21dabd75f91420346cf6bde4b6e42615e9b0dce
Brian Zwahr <echosa@icloud.com>
2018-11-08 15:39:58 -0600

    Add PHPSpec and UsersSpec.

---
+++ spec/TwitchApi/NewApi/Resources/UsersSpec.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace spec\TwitchApi\NewApi\Resources;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use PhpSpec\ObjectBehavior;
+use TwitchApi\NewApi\RequestResponse;
+use TwitchApi\NewApi\Resources\Users;
+
+class UsersSpec extends ObjectBehavior
+{
+    function let(Client $guzzleClient)
+    {
+        $this->beConstructedWith($guzzleClient);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(Users::class);
+    }
+
+    function it_should_get_users_by_ids(Client $guzzleClient, Response $response)
+    {
+        $guzzleClient->send(new Request('GET', 'users?id=12345&id=98765'))->willReturn($response);
+        $this->getUsers([12345,98765])->shouldBeAnInstanceOf(RequestResponse::class);
+    }
+
+    function it_should_get_users_by_usernames(Client $guzzleClient, Response $response)
+    {
+        $guzzleClient->send(new Request('GET', 'users?login=twitchuser&login=anotheruser'))->willReturn($response);
+        $this->getUsers([], ['twitchuser','anotheruser'])->shouldBeAnInstanceOf(RequestResponse::class);
+    }
+
+    function it_should_get_users_by_id_and_username(Client $guzzleClient, Response $response)
+    {
+        $guzzleClient->send(new Request('GET', 'users?id=12345&id=98765&login=twitchuser&login=anotheruser'))->willReturn($response);
+        $this->getUsers([12345,98765], ['twitchuser','anotheruser'])->shouldBeAnInstanceOf(RequestResponse::class);
+    }
+
+    function it_should_get_a_single_user_by_id(Client $guzzleClient, Response $response)
+    {
+        $guzzleClient->send(new Request('GET', 'users?id=12345'))->willReturn($response);
+        $this->getUserById(12345)->shouldBeAnInstanceOf(RequestResponse::class);
+    }
+
+    function it_should_get_a_single_user_by_username(Client $guzzleClient, Response $response)
+    {
+        $guzzleClient->send(new Request('GET', 'users?login=twitchuser'))->willReturn($response);
+        $this->getUserByUsername('twitchuser')->shouldBeAnInstanceOf(RequestResponse::class);
+    }
+
+    function it_should_get_users_follows_by_follower_id(Client $guzzleClient, Response $response)
+    {
+        $guzzleClient->send(new Request('GET', 'users/follows?from_id=12345'))->willReturn($response);
+        $this->getUsersFollows(12345)->shouldBeAnInstanceOf(RequestResponse::class);
+    }
+
+    function it_should_get_users_follows_by_followed_id(Client $guzzleClient, Response $response)
+    {
+        $guzzleClient->send(new Request('GET', 'users/follows?to_id=12345'))->willReturn($response);
+        $this->getUsersFollows(null, 12345)->shouldBeAnInstanceOf(RequestResponse::class);
+    }
+
+    function it_should_get_users_follows_by_follower_id_and_followed_id(Client $guzzleClient, Response $response)
+    {
+        $guzzleClient->send(new Request('GET', 'users/follows?from_id=12345&to_id=98765'))->willReturn($response);
+        $this->getUsersFollows(12345, 98765)->shouldBeAnInstanceOf(RequestResponse::class);
+    }
+}

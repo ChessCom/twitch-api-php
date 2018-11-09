a90c8bfa11451eb4ced4331059e10371ecf38556
Brian Zwahr <echosa@icloud.com>
2018-11-09 10:47:04 -0600

    Add another unit test.

--- test/NewTwitchApi/Resources/UsersTest.php
+++ test/NewTwitchApi/Resources/UsersTest.php
@@ -12,7 +12,36 @@ use NewTwitchApi\Resources\Users;
 
 class UsersTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGetUserByIdShouldReturnSuccessfulResponseWithUserData()
+    public function testGetUserByIdShouldReturnSuccessfulResponseWithUserData(): void
+    {
+        $users = new Users($this->getGuzzleClientWithMockUserResponse());
+        $requestResponse = $users->getUserById(44322889);
+
+        $request = $requestResponse->getRequest();
+        $this->assertEquals('users?id=44322889', $request->getUri());
+
+        $response = $requestResponse->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+        $contents = json_decode($response->getBody()->getContents());
+        $this->assertEquals('dallas', $contents->data[0]->login);
+    }
+
+    public function testGetUserByUsernameShouldReturnSuccessfulResponseWithUserData(): void
+    {
+
+        $users = new Users($this->getGuzzleClientWithMockUserResponse());
+        $requestResponse = $users->getUserByUsername('dallas');
+
+        $request = $requestResponse->getRequest();
+        $this->assertEquals('users?login=dallas', $request->getUri());
+
+        $response = $requestResponse->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+        $contents = json_decode($response->getBody()->getContents());
+        $this->assertEquals(44322889, $contents->data[0]->id);
+    }
+
+    private function getGuzzleClientWithMockUserResponse(): Client
     {
         // Example response from https://dev.twitch.tv/docs/api/reference/#get-users
         $getUserReponseJson = <<<JSON
@@ -35,17 +64,6 @@ JSON;
         $db_response = new Response(200, [], $getUserReponseJson);
         $mock = new MockHandler([ $db_response, $db_response ]);
         $handler = HandlerStack::create($mock);
-        $guzzleClient = new Client(['handler' => $handler]);
-
-        $users = new Users($guzzleClient);
-        $requestResponse = $users->getUserById(44322889);
-
-        $request = $requestResponse->getRequest();
-        $this->assertEquals('users?id=44322889', $request->getUri());
-
-        $response = $requestResponse->getResponse();
-        $this->assertEquals(200, $response->getStatusCode());
-        $contents = json_decode($response->getBody()->getContents());
-        $this->assertEquals('dallas', $contents->data[0]->login);
+        return new Client(['handler' => $handler]);
     }
 }
